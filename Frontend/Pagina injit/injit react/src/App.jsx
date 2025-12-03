560d90e7eefc07bdddfca478e6ac77d626ee79ec
Cosmic_ <jordanlagos@outlook.com>
2025-12-03 01:51:56 -0300

    nuevos cambios

--- Frontend/Pagina injit/injit react/src/App.jsx
+++ Frontend/Pagina injit/injit react/src/App.jsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
-
+import Main from "./pages/Main"
 import AppProdu from "./pages/AppProdu";
 import Razas from "./pages/Razas";
 import Purezas from "./pages/Purezas";
 import Login from "./pages/Login";
 
 import Header from "./components/Header";
-import Footer from "../components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/AppProdu" element={<AppProdu />} />
         <Route path="/razas" element={<Razas />} />
         <Route path="/purezas" element={<Purezas />} />
