fcdcc6fc459f622bb4461e5d73e3384ea21ff9a7
Cosmic_ <jordanlagos@outlook.com>
2025-12-03 02:01:24 -0300

    link de main cambiado en index.html

--- Frontend/Pagina injit/injit react/src/App.jsx
+++ Frontend/Pagina injit/injit react/src/App.jsx
@@ -1,19 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
+import Main from "./pages/main"
 import AppProdu from "./pages/AppProdu";
 import Razas from "./pages/Razas";
 import Purezas from "./pages/Purezas";
 import Login from "./pages/Login";
 
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-
+        <Route path="/" element={<Main />} />
         <Route path="/AppProdu" element={<AppProdu />} />
         <Route path="/razas" element={<Razas />} />
         <Route path="/purezas" element={<Purezas />} />
