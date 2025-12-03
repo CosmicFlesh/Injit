57ef96ebba8e6864c4fd716eafd57a578605e5b1
Cosmic_ <jordanlagos@outlook.com>
2025-12-03 02:21:17 -0300

    lo mismo de ante pero en login.jsx

--- Frontend/Pagina injit/injit react/src/pages/login.jsx
+++ Frontend/Pagina injit/injit react/src/pages/login.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header.js'
-import Footer from './components/Footer.js'
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
 import User from '../fomulario user.jsx'
 
 
